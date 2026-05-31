74bc80b03394b6af9ebc2a26a1caefea4cc3a972
vismaya <vichuvismaya422@gmail.com>
2026-05-31 21:28:22 +0530

    employee_directory

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery, name='gallery'),
-    path('', views.contact, name='contact'),
+    path('', views.employee_directory, name='employees'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 
-def gallery(request):
-    return render(request, 'gallery.html')
+def employee_directory(request):
+    employees = [
+        {
+            'name': 'Manu',
+            'job_title': 'Software Engineer',
+            'salary': 60000,
+            'is_full_time': True
+        },
+        {
+            'name': 'Amal',
+            'job_title': 'Designer',
+            'salary': 35000,
+            'is_full_time': False
+        },
+        {
+            'name': 'Arjun',
+            'job_title': 'Manager',
+            'salary': 80000,
+            'is_full_time': True
+        }
+    ]
 
-def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'employee_directory.html', {
+        'employees': employees
+    })
